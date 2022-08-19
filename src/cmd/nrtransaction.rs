6e1f3ad7c0d7ea0bdde03ecc8eb5a7500654c125
Vladimir Ulogov <vladimir.ulogov@me.com>
2022-08-19 16:27:06 -0600

    Dev #8

--- src/cmd/nrtransaction.rs
+++ src/cmd/nrtransaction.rs
@@ -29,46 +29,7 @@ pub fn process_end(c: &cmd::Cli, error: &String, service: &String, trace_id: &St
     } else {
         log::debug!("Transaction {} has been found", &tx_id);
     }
-    let mut res = Map::new();
-    let mut attr = Map::new();
-    attr.insert("host".to_string(), Value::from(c.hostname.as_str()));
-    res.insert("timestamp".to_string(), json!(c.timestamp));
-    res.insert("trace.id".to_string(), Value::from(t.as_str()));
-    res.insert("id".to_string(), Value::from(i.as_str()));
-    if ! p.is_empty() {
-        attr.insert("parent.id".to_string(), Value::from(p.as_str()));
-    }
-    attr.insert("name".to_string(), Value::from(n.as_str()));
-    attr.insert("duration.ms".to_string(), json!(d));
-    attr.insert("primary_application_id".to_string(), Value::from(format!("{}-{}", n, i)));
-    attr.insert("service.name".to_string(), Value::from(s.as_str()));
-    attr.insert("sampled".to_string(), json!(sampled));
-    attr.insert("type".to_string(), Value::from(trace_type.as_str()));
-    attr.insert("category".to_string(), Value::from(trace_category.as_str()));
-    if ! e.is_empty() {
-        attr.insert("error.message".to_string(), Value::from(e.as_str()));
-    }
-    if ! instance_id.is_empty() {
-        attr.insert("service.instance.id".to_string(), Value::from(instance_id.as_str()));
-    }
-    for l in a {
-        let pair: Vec<_> = l.splitn(2, "=").collect();
-        if pair.len() != 2 {
-            continue;
-        }
-        let key = pair[0];
-        let value = pair[1];
-        attr.insert(key.to_string(), cmd::string_to_value(&c, &c.eval, value));
-    }
-    res.insert("attributes".to_string(), json!(&attr));
-    let out = json!([{
-        "spans": [
-            &res,
-        ]
-    }]);
-    let payload = to_string(&out).unwrap();
-    log::debug!("{}", &payload);
-    send_trace(c, &payload);
+    
     nrkv::delete(&c, &tx_id)
 }
 
